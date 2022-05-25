0b02c80573f16b1dc5a61faa445b829a6f7ff786
js.lozano@globant.com <142308Jslp>
2022-05-25 14:49:34 -0500

    Refactor to enum user class

--- src/test/java/serenityswag/authentication/WhenLoggingOn.java
+++ src/test/java/serenityswag/authentication/WhenLoggingOn.java
@@ -1,16 +1,17 @@
 package serenityswag.authentication;
 
-import net.serenitybdd.core.steps.UIInteractionSteps;
+import net.serenitybdd.core.Serenity;
 import net.serenitybdd.junit5.SerenityJUnit5Extension;
 import net.thucydides.core.annotations.Managed;
 import net.thucydides.core.annotations.Steps;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import serenityswag.authentication.actions.LoginActions;
 import serenityswag.inventory.InventoryPage;
+
+import static serenityswag.authentication.User.STANDARD_USER;
 
 @ExtendWith(SerenityJUnit5Extension.class)
 public class WhenLoggingOn {
@@ -25,9 +26,10 @@ public class WhenLoggingOn {
 
     @Test
     public void usersCanLogOnViaTheHomePage() {
-        login.asAStandardUser();
+        login.as(STANDARD_USER);
         // Should see product catalog
-        Assertions.assertThat(inventoryPage.getHeading())
-                .isEqualToIgnoringCase("Products");
+        Serenity.reportThat("The inventory page should be displayed with correct title",
+                ()->Assertions.assertThat(inventoryPage.getHeading())
+                        .isEqualToIgnoringCase("Products"));
     }
 }
--- src/test/java/serenityswag/authentication/actions/LoginActions.java
+++ src/test/java/serenityswag/authentication/actions/LoginActions.java
@@ -2,16 +2,17 @@ package serenityswag.authentication.actions;
 
 import net.serenitybdd.core.steps.UIInteractionSteps;
 import net.thucydides.core.annotations.Step;
+import serenityswag.authentication.User;
 
 public class  LoginActions extends UIInteractionSteps {
 
-    @Step("Log in as a standard user")
-    public void asAStandardUser(){
+    @Step("Log in as {0}")
+    public void as(User user){
         openUrl("https://www.saucedemo.com/");
 
         // Login as a standard user
-        $("[data-test='username']").sendKeys("standard_user");
-        $("[data-test='password']").sendKeys("secret_sauce");
+        $("[data-test='username']").sendKeys(user.getUsername());
+        $("[data-test='password']").sendKeys(user.getPassword());
         $("[data-test='login-button']").click();
     }
 
