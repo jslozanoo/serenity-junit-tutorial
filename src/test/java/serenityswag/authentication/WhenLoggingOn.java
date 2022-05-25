c47ba44a8122dded14502968133d91bfdc7dba02
js.lozano@globant.com <142308Jslp>
2022-05-25 13:57:10 -0500

    Refactor with Page Object

--- src/test/java/serenityswag/authentication/WhenLoggingOn.java
+++ src/test/java/serenityswag/authentication/WhenLoggingOn.java
@@ -3,29 +3,31 @@ package serenityswag.authentication;
 import net.serenitybdd.core.steps.UIInteractionSteps;
 import net.serenitybdd.junit5.SerenityJUnit5Extension;
 import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.Steps;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import serenityswag.authentication.actions.LoginActions;
+import serenityswag.inventory.InventoryPage;
 
 @ExtendWith(SerenityJUnit5Extension.class)
-public class WhenLoggingOn extends UIInteractionSteps {
+public class WhenLoggingOn {
 
     @Managed
     WebDriver driver;
 
+    @Steps
+    LoginActions login;
+
+    InventoryPage inventoryPage;
+
     @Test
     public void usersCanLogOnViaTheHomePage() {
-        openUrl("https://www.saucedemo.com/");
-
-        // Login as a standard user
-        find("[data-test='username']").sendKeys("standard_user");
-        find("[data-test='password']").sendKeys("secret_sauce");
-        find("[data-test='login-button']").click();
-
+        login.asAStandardUser();
         // Should see product catalog
-        Assertions.assertThat(find(".title").getText())
+        Assertions.assertThat(inventoryPage.getHeading())
                 .isEqualToIgnoringCase("Products");
     }
 }
