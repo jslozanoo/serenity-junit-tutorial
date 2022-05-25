be51afce2644dc91dc4b9adafb7a910de8143182
js.lozano@globant.com <142308Jslp>
2022-05-25 13:14:37 -0500

    Refactor with steps

--- src/test/java/serenityswag/authentication/WhenLoggingOn.java
+++ src/test/java/serenityswag/authentication/WhenLoggingOn.java
@@ -1,5 +1,6 @@
 package serenityswag.authentication;
 
+import net.serenitybdd.core.steps.UIInteractionSteps;
 import net.serenitybdd.junit5.SerenityJUnit5Extension;
 import net.thucydides.core.annotations.Managed;
 import org.assertj.core.api.Assertions;
@@ -9,22 +10,22 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 @ExtendWith(SerenityJUnit5Extension.class)
-public class WhenLoggingOn {
+public class WhenLoggingOn extends UIInteractionSteps {
 
     @Managed
     WebDriver driver;
 
     @Test
     public void usersCanLogOnViaTheHomePage() {
-        driver.get("https://www.saucedemo.com/");
+        openUrl("https://www.saucedemo.com/");
 
         // Login as a standard user
-        driver.findElement(By.cssSelector("[data-test='username']")).sendKeys("standard_user");
-        driver.findElement(By.cssSelector("[data-test='password']")).sendKeys("secret_sauce");
-        driver.findElement(By.cssSelector("[data-test='login-button']")).click();
+        find("[data-test='username']").sendKeys("standard_user");
+        find("[data-test='password']").sendKeys("secret_sauce");
+        find("[data-test='login-button']").click();
 
         // Should see product catalog
-        Assertions.assertThat(driver.findElement(By.cssSelector(".title")).getText())
+        Assertions.assertThat(find(".title").getText())
                 .isEqualToIgnoringCase("Products");
     }
 }
