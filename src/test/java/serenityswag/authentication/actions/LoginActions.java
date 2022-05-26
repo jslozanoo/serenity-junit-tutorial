f36e6c26deed04f2d3549c5b8d478cc0303f689f
js.lozano@globant.com <142308Jslp>
2022-05-26 11:22:36 -0500

    Id locator completed

--- src/test/java/serenityswag/authentication/actions/LoginActions.java
+++ src/test/java/serenityswag/authentication/actions/LoginActions.java
@@ -2,6 +2,7 @@ package serenityswag.authentication.actions;
 
 import net.serenitybdd.core.steps.UIInteractionSteps;
 import net.thucydides.core.annotations.Step;
+import org.openqa.selenium.By;
 import serenityswag.authentication.User;
 
 public class  LoginActions extends UIInteractionSteps {
@@ -10,10 +11,17 @@ public class  LoginActions extends UIInteractionSteps {
     public void as(User user){
         openUrl("https://www.saucedemo.com/");
 
-        // Login as a standard user
+        find(By.id("user-name")).sendKeys(user.getUsername());
+        find(By.id("password")).sendKeys(user.getPassword());
+        find(By.id("login-button")).click();
+
+        // Login as a standard user}
+        /*
         $("[data-test='username']").sendKeys(user.getUsername());
         $("[data-test='password']").sendKeys(user.getPassword());
         $("[data-test='login-button']").click();
+
+         */
     }
 
 }
